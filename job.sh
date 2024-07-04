fefff906d7f4e162d6b9932ddf41a6b9bda52e5f
jonas-nothnagel <jonas.nothnagel@gmail.com>
2024-07-04 09:52:18 +0200

    ?

--- job.sh
+++ job.sh
@@ -10,4 +10,4 @@ curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.s
 apt-get install git-lfs
 git lfs install
 # run training script unbuffered
-python -u ./autorag/ragas.py 
+python -u ./autorag/ragas_test_data.py 
